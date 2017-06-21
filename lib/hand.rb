a06dd4f50b45d05e2461fbe5b2ce5e53b6489ec1
Manu Kanthan <m.kanthan@gmail.com>
2017-06-20 22:25:20 -0400

    Majority of the rules - missing high card and scoring with high card (so there’s no tie)

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,13 +1,64 @@
 class Hand
+  ROYAL_SUM = 60
+  HIGH_ACE_NUMBER = 14
+
   def initialize(hand)
     @cards = hand.split
-    @numbers = @cards.map { |c| c[0] }.sort
+    @numbers = @cards.map { |c| to_integer(c[0]) }.sort
+    @counts = @numbers.group_by{ |x| x }.map{|k, v| [k, v.length]}.to_h
     @suit = @cards.map { |c| c[1] }
   end
 
   def rank
-    return Rank::ROYAL_FLUSH if (@numbers.join == "AJKQT" && @suit.uniq.length == 1)
+    royal = is_royal
+    flush = is_flush
+    straight = is_straight
+    four_of_a_kind = is_x_of_a_kind(4)
+    three_of_a_kind = is_x_of_a_kind(3)
+    one_pair = is_x_of_a_kind(2)
+    return Rank::ROYAL_FLUSH      if (royal && flush)
+    return Rank::STRAIGHT_FLUSH   if (straight && flush)
+    return Rank::FOUR_OF_A_KIND   if (four_of_a_kind)
+    return Rank::FULL_HOUSE       if (three_of_a_kind && one_pair)
+    return Rank::FLUSH            if (flush)
+    return Rank::STRAIGHT         if (straight)
+    return Rank::THREE_OF_A_KIND  if (three_of_a_kind)
+    return Rank::TWO_PAIR         if (has_three_counts && one_pair)
+    return Rank::ONE_PAIR         if (one_pair)
   end
 
+  private
+  def to_integer(card)
+    face_map = { "T" => 10, "J" => 11, "Q" => 12, "K" => 13, "A" => 14 }
+    face_map[card] || card.to_i
+  end
 
+  def is_royal
+    @numbers.inject(0){|sum,x| sum + x } == ROYAL_SUM
+  end
+
+  def is_flush
+    @suit.uniq.length == 1
+  end
+
+  def is_straight
+    straight = (@numbers.first..@numbers.last).to_a == @numbers
+    low_ace_straight = false
+    if (@numbers.include?(HIGH_ACE_NUMBER))
+      low_ace_numbers = @numbers.dup.shift(4).unshift(1)
+      low_ace_straight = (low_ace_numbers.first..low_ace_numbers.last).to_a == low_ace_numbers
+    end
+    straight || low_ace_straight
+  end
+
+  def is_x_of_a_kind(x)
+    @counts.values.each do |value|
+      return true if value == x
+    end
+    return false
+  end
+
+  def has_three_counts
+    @counts.values.length == 3
+  end
 end
--- spec/lib/hand_spec.rb
+++ spec/lib/hand_spec.rb
@@ -11,5 +11,69 @@ RSpec.describe Hand do
       @hand = Hand.new("TS QS JS AS KS")
       expect(@hand.rank).to eq(Rank::ROYAL_FLUSH)
     end
+
+    it "should return a straight flush for a straight flush hand" do
+      @hand = Hand.new("AD 2D 5D 3D 4D")
+      expect(@hand.rank).to eq(Rank::STRAIGHT_FLUSH)
+
+      @hand = Hand.new("8S 9S TS QS JS")
+      expect(@hand.rank).to eq(Rank::STRAIGHT_FLUSH)
+    end
+
+    it "should return a four of a kind for a four of a kind" do
+      @hand = Hand.new("AH AC 3D AS AD")
+      expect(@hand.rank).to eq(Rank::FOUR_OF_A_KIND)
+
+      @hand = Hand.new("7S 9S 7D 7H 7C")
+      expect(@hand.rank).to eq(Rank::FOUR_OF_A_KIND)
+    end
+
+    it "should return a full house for a three of a kind and one pair" do
+      @hand = Hand.new("AH 3C AD 3S 3D")
+      expect(@hand.rank).to eq(Rank::FULL_HOUSE)
+
+      @hand = Hand.new("7S 9S 7D 7H 9C")
+      expect(@hand.rank).to eq(Rank::FULL_HOUSE)
+    end
+
+    it "should return a flush for a flush" do
+      @hand = Hand.new("AH 8H 2H JH QH")
+      expect(@hand.rank).to eq(Rank::FLUSH)
+
+      @hand = Hand.new("7S 9S 8S 2S JS")
+      expect(@hand.rank).to eq(Rank::FLUSH)
+    end
+
+    it "should return a straight for a straight" do
+      @hand = Hand.new("3H AD 4C 2C 5S")
+      expect(@hand.rank).to eq(Rank::STRAIGHT)
+
+      @hand = Hand.new("6C 7C TD 8D 9H")
+      expect(@hand.rank).to eq(Rank::STRAIGHT)
+    end
+
+    it "should return three of a kind for a three of a kind" do
+      @hand = Hand.new("AH AC 3C AD 2D")
+      expect(@hand.rank).to eq(Rank::THREE_OF_A_KIND)
+
+      @hand = Hand.new("4H 3C 4C 4D 2D")
+      expect(@hand.rank).to eq(Rank::THREE_OF_A_KIND)
+    end
+
+    it "should return two pair for two pair" do
+      @hand = Hand.new("AH AC 3C 2H 2D")
+      expect(@hand.rank).to eq(Rank::TWO_PAIR)
+
+      @hand = Hand.new("8H 3C 4C 3D 4D")
+      expect(@hand.rank).to eq(Rank::TWO_PAIR)
+    end
+
+    it "should return one pair for just one pair" do
+      @hand = Hand.new("AH AC 3D 4S 5D")
+      expect(@hand.rank).to eq(Rank::ONE_PAIR)
+
+      @hand = Hand.new("AD 9S 3D 3H 7C")
+      expect(@hand.rank).to eq(Rank::ONE_PAIR)
+    end
   end
 end
